d7acd2685f77e91bbf0e5a102c25d8eaf8cb4b70
Moe Aoki <mt215tjk@gmail.com>
2020-08-04 16:41:41 +0900

    No confirmation for destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # index, show, new, create, update, edit, destroy
+  # Read
+  get '/tasks', to: 'tasks#index'
+  # New
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # Create
+  post '/tasks', to: 'tasks#create'
+  # Update
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+  # Destroy
+  delete '/tasks/:id', to: 'tasks#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
